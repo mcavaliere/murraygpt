348d9a4bc48153a87e713502d23a24a57f6c2f30
Mike Cavaliere <mcavaliere@echobind.com>
2023-04-19 13:00:42 -0400

    feat: quote card component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import { startTransition, useState, useEffect } from "react";
 
 import { OpenAI } from "@/lib/OpenAI";
+import { QuoteCard } from "@/components/QuoteCard";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -56,11 +57,11 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="border border-gray-900 relative flex place-items-center after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px] w-full">
+      <div className="relative flex justify-center items-center place-items-center after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px] w-full h-80">
         {loading ? (
           <h2>Loading...</h2>
         ) : murrayism ? (
-          <blockquote>{murrayism}</blockquote>
+          <QuoteCard quote={murrayism} author="Bill Murray" />
         ) : null}
       </div>
       <div className="z-10 w-full max-w-5xl items-center justify-end font-mono text-sm lg:flex ">
